9209d187bc45d1c6e44cf4543569958e40cbf4ef
Gowtham Muralivel <gowtham.muralivel@mallow-tech.com>
2021-03-02 21:31:13 +0530

    initial project setup

---
+++ app/src/main/java/com/gmg/learning/pagination/splashscreen/GMSplashActivity.kt
@@ -0,0 +1,14 @@
+package com.gmg.learning.pagination.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmg.learning.pagination.R
+
+class GMSplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gm_splash_activity)
+    }
+
+}
